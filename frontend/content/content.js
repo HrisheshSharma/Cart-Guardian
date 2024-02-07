ef6cc8c9813243542ef93776237f3e2677e4118d
Krish Agrawal <diwankrish.17@gmail.com>
2024-02-07 12:04:39 +0530

    frontend

---
+++ frontend/content/content.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+  let text = document.body.outerHTML;
+  console.log(text);
+
+  fetch("http://127.0.0.1:8000/page", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ pageData: text, pageUrl: window.location.href, pageTitle: document.title}),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  chrome.runtime.sendMessage({
+      message: 'text',
+      payload: text
+  });
+};
